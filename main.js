56b62c709d700c572dc6a7a8bb182eaa7785bb8c
UriceMergulhao <80512744+UriceMergulhao@users.noreply.github.com>
2022-01-12 16:21:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+function setup(){
+    canvas=createCanvas(280,280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth=window.speechSynthesis;
+
+
+}
+
+function clearCanvas(){
+    background("white");
+}
+
+function preload(){
+    classifier=ml5.imageClassifier('DoodleNet')
+}
+
+function draw(){
+    strokeWaight(12);
+stroke(0);
+if(mouseIsPressed){
+    line(pmouseX,pmouseY,mouseX,mouseY);
+}
+}
+
+function classifyCanvas(){
+    classifier.classify(canvas,gotResult);
+
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+
+    }
+console.log(results);
+document.getElementById("label").innerHTML="label- "+results[0].label;
+document.getElementById("confidence").innerHTML="confidence- "+results[0].confidence.tofixed(3);
+utterThis=new SpeechSynthesisUtterance(results[0].lable);
+synth.speek(utterThis);
+}
+    
+
+
